ee6e58b509bb580df458a873a55f66f5a6d0d51e
friday <873384958@qq.com>
2015-07-02 13:28:05 +0800

    齐全

---
+++ Gruntfile.js
@@ -0,0 +1,91 @@
+module.exports = function(grunt) {
+
+    // 任务配置,所有插件的配置信息
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        all: {
+            options: {
+                port: 8080,
+                hostname: "localhost",
+                bases: ['.'],
+                livereload: true
+            }
+        },
+
+        // uglify插件的配置信息
+        uglify: {
+            build: {
+                src: 'src/a.js',
+                dest: 'build/a.min.js'
+            }
+        },
+        cssmin: {
+            //文件头部输出信息
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n',
+                //美化代码
+                beautify: {
+                    //中文ascii化，非常有用！防止中文乱码的神配置
+                    ascii_only: true
+                }
+            },
+            my_target: {
+                files: [{
+                    expand: true,
+                    //相对路径
+                    cwd: 'output/',
+                    src: '*.css',
+                    dest: 'build/'
+                }]
+            }
+        },
+        less: {
+            lessall: {
+                files: [{
+                    expand: true,
+                    cwd: 'src',
+                    src: '**/*.less',
+                    dest: 'output/',
+                    ext: '.css'
+                }]
+            }
+        },
+        watch: {
+            less: {
+                files: ['**/*.less'],
+                tasks: ['less']
+            },
+            client: {
+                files: ['*.html', 'output/*', 'js/*', 'images/**/*'],
+                options: {
+                    livereload: true
+                }
+            }
+        },
+        imagemin: {
+            /* 压缩图片大小 */
+            dist: {
+                options: {
+                    optimizationLevel: 3 // png图片优化水平，3是默认值，取值区间0-7
+                },
+                files: [{
+                    expand: true,
+                    cwd: 'images/',
+                    src: ['**/*.{png,jpg,jpeg,gif}'], // 优化 img 目录下所有 png/jpg/jpeg/gif图片
+                    dest: 'img/' // 优化后的图片保存位置，覆盖旧图片，并且不作提示
+                }]
+            }
+        },
+    });
+
+    // 告诉grunt我们将使用插件
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-cssmin');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
+    // 告诉grunt当我们在终端中输入grunt时需要做些什么
+    grunt.registerTask('default', ['uglify', 'less', 'cssmin', 'imagemin']);
+    grunt.registerTask('live', ['watch']);
+
+};
